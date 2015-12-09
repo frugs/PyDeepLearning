32379a5aa448158ea7f4fa6283362f3f35269bbb
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-12-09 00:38:59 +0000

    removed license data file from setup script

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='PyDeepLearning',
-    version='0.1',
+    version='0.1.1',
     packages=['pydl'],
     url='https://github.com/frugs/PyDeepLearning',
     license='MIT',
     author='hugo',
     author_email='frugs@github.com',
     description='Deep learning neural network implementation with backprop using numpy.',
-    data_files=[("", ["LICENSE.txt"])],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: MIT License',
                  'Programming Language :: Python :: 3'],
